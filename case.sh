2a3f0ed275513d6facc2c688db2a455aefb1072b
kemo-byte <kamalkafi12@gmail.com>
2022-02-13 23:05:16 +0200

    case in shell

--- case.sh
+++ case.sh
@@ -3,10 +3,18 @@
 echo "Type a number"
 read number
 
-if [ "$number" -eq "1" ]; then
-	echo "one"
-elif [  "$number" -eq "0" ]; then
-	echo "zero"
-else
-	echo "wrong input"
-fi
+# if [ "$number" -eq "1" ]; then
+# 	echo "one"
+# elif [  "$number" -eq "0" ]; then
+# 	echo "zero"
+# else
+# 	echo "wrong input"
+# fi
+
+case $number in
+	1) echo "one"
+		;;
+	0) echo "zero"
+		;;
+	*) echo "wrong"
+esac
