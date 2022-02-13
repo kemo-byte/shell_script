5de2447793a7959a2f804cc5e424a55da24f0957
kemo-byte <kamalkafi12@gmail.com>
2022-02-13 21:38:34 +0200

    else if and short hand

--- shellif.sh
+++ shellif.sh
@@ -5,8 +5,19 @@ echo "Type The Degree"
 read degree
 
 # gt => (>) lt => (<) ge => (>=) ne => (!=) 
-if [[ "degree" -ge "50" ]]; then
+if [[ "$degree" -ge "50" ]]
+then
 	echo "successfull !"
 else
 	echo "failed !"
 fi
+
+# if [[ "$degree" -ge "80" ]];then
+
+# 	echo "he go 80"
+# elif [[ "$degree" -ge "70" ]]; then
+# 	echo "he got 70"
+
+# fi
+
+[ "$degree" -ge "80" ] && echo "got 80 " || echo "less than 80"
